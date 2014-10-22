5d074d4f5d67c48383e96a9b8ce943321f9311ac
Rahul Verma <mail2rahul911@gmail.com>
2014-10-22 12:27:17 +0530

    Home Page Added

--- app/src/main/java/echaiventures/designyourcity/HomePage.java
+++ app/src/main/java/echaiventures/designyourcity/HomePage.java
@@ -1,12 +1,18 @@
 package echaiventures.designyourcity;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import echaiventures.designyourcity.issue.IssueMain;
 
@@ -18,6 +24,7 @@ public class HomePage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        changeActionBar();
 
         btnDiscussion =(Button)findViewById(R.id.button1);
         btnIssue = (Button)findViewById(R.id.button2);
@@ -39,20 +46,19 @@ public class HomePage extends Activity {
         });
     }
 
+    private void changeActionBar() {
+        ActionBar myActionBar = getActionBar();
+        myActionBar.setDisplayShowHomeEnabled(false);
+        myActionBar.setDisplayShowTitleEnabled(false);
 
+        LayoutInflater myInflater = LayoutInflater.from(this);
+        View myCustomView = myInflater.inflate(R.layout.custom_main_title_bar, null);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.home_page, menu);
-        return true;
+        TextView myTitleText = (TextView) myCustomView.findViewById(R.id.tvTitleText);
+        myTitleText.setText("Design Your City");
+
+        myActionBar.setCustomView(myCustomView);
+        myActionBar.setDisplayShowCustomEnabled(true);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
